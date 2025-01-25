684fe26ccf9fb3e74e3f017644415f817225ec2c
McKenzie Hughes <mhughe57@ut.utm.edu>
2025-01-24 23:30:08 -0600

    Second Commit: sum that takes the integer as a parameter...

--- main.cpp
+++ main.cpp
@@ -4,8 +4,20 @@
 
 using namespace std;
 
+int sum(int n){
+  int sum = 0;
+  for(int i = 1; i <= n; i++){
+    sum += 1;
+  }
+  return sum;
+}
+
+
 int main(){
-  cout<< "Hello World";
+  int n;
+  cout << "Please input a number: " ;
+  cin >> n;
+  cout << "The Sum of numbers from 1 to " << n << " is " << sum(n) << "." << endl; 
   return 0;
 }
 
