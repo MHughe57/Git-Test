326a6e96b686d2a7d1c09f0b9ec4560096b686dc
McKenzie Hughes <mhughe57@ut.utm.edu>
2025-01-24 23:37:26 -0600

    First commit: New branch and added product func.

--- main.cpp
+++ main.cpp
@@ -12,12 +12,23 @@ int sum(int n){
   return sum;
 }
 
+int product(int n){
+  int product = 1;
+  
+  for(int i = 1; i <= n; i++){
+    product *= 1;
+  }
+  return product;
+}
 
 int main(){
   int n;
   cout << "Please input a number: " ;
   cin >> n;
-  cout << "The Sum of numbers from 1 to " << n << " is " << sum(n) << "." << endl; 
+  
+  cout << "The Sum of numbers from 1 to " << n << " is " << sum(n) << "." << endl;
+  cout << "The product of numbers from 1 to " << n << " is " << product(n) << "." << endl;
+  
   return 0;
 }
 
